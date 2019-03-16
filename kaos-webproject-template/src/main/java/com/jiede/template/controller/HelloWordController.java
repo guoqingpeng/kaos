1d940c7a0f106c0548b507de376634da5f30a0cf
kakuseihou <xiaogqp@163.com>
2019-03-16 13:33:10 +0800

    add springmvc ability

---
+++ kaos-webproject-template/src/main/java/com/jiede/template/controller/HelloWordController.java
@@ -0,0 +1,19 @@
+package com.jiede.template.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWordController {
+
+	@ResponseBody
+	@RequestMapping("hh")
+	public String hello() {
+		
+		return "hello kaku";
+		
+	}
+	
+
+}
---
+++ kaos-webproject-template/src/main/java/com/jiede/template/controller/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author guoqingpeng
+ *
+ */
+package com.jiede.template.controller;
